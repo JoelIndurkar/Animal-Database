710c21329e951522f51aeae0c4a89b5fcc695dc4
Joel Indurkar <joel.indurkar@gmail.com>
2018-12-11 17:39:23 +0000

    php possible fix

--- PHPtest.php
+++ PHPtest.php
@@ -4,13 +4,13 @@
     $host = "127.0.0.1";
     $user = "joelindurkar";                     //Your Cloud 9 username
     $pass = "";                                  //Remember, there is NO password by default!
-    $db = "animalprojectdb";                                  //Your database name you want to connect to
+    $db = "project_db";                                  //Your database name you want to connect to
     $port = 3306;                                //The port #. It is always 3306
     
     $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
 
 //And now to perform a simple query to make sure it's working
-    $query = "SELECT * FROM users";
+    $query = "SELECT * FROM animals";
     $result = mysqli_query($connection, $query);
 
     while ($row = mysqli_fetch_assoc($result)) {
