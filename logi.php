3976bbe761b9432c22b07563bd7096fcadd2b42b
Joel Indurkar <joel.indurkar@gmail.com>
2019-01-07 18:17:29 +0000

    final project

---
+++ logi.php
@@ -0,0 +1,11 @@
+<?php
+    
+    //Connect to the database
+    $host = "127.0.0.1";
+    $user = "joelindurkar";                     //Your Cloud 9 username
+    $pass = "";                                  //Remember, there is NO password by default!
+    $db = "project_db";                                  //Your database name you want to connect to
+    $port = 3306;                                //The port #. It is always 3306
+    
+    $connection = mysqli_connect($host, $user, $pass, $db, $port)or die(mysql_error());
+?>
--- phpUse.php
+++ phpUse.php
@@ -1,6 +1,6 @@
 <?php
         include_once 'WebsitePart1.html';
-        recquire_once 'phpTest.php';
+        require_once 'phpTest.php';
         $conn = mysqli_connect($host, $user, $pass, $db, $port);
         if($conn->connect_error) die($conn->connect_error);
         $option = isset($_POST['myTest']) ?$_POST['myTest'] : false;
@@ -10,7 +10,7 @@
           $query = " select ". $testField . " from project_db;";
           //echo $query;
           $result = mysqli_query($conn, $query);
-          while ($row = my sqli_fetch_assoc($result)) {
+          while ($row = mysqli_fetch_assoc($result)) {
             echo $row[$testField] . "<br>" ;
 
           }
